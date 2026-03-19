61d85cae6b12ee8ccf96157037001cefc35bbb27
Hash <hashiramanakamoto0@gmail.com>
2026-03-19 08:40:52 +0000

    Perfeito, tudo certo. Agora corrija: 05:18:43.971 Running build in Washi

--- src/modules/explore/components/CandidateCard.tsx
+++ src/modules/explore/components/CandidateCard.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import Link from 'next/navigation';
+import Link from 'next/link';
 import Image from 'next/image';
 import { ArrowRight, Star, ThumbsUp } from 'lucide-react';
 import { StatusIndicator } from './StatusIndicator';
