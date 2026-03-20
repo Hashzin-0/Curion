00ed93f931abb3bede4ac3432cb4ab57ea876024
Hash <hashiramanakamoto0@gmail.com>
2026-03-20 17:22:37 +0000

    s

--- src/app/[username]/[area_slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-// @deprecated - Use a rota CamelCase [areaSlug] para evitar conflitos de build no Next.js
-import { redirect } from 'next/navigation';
-
-export default async function Page({ params }: any) {
-  const resolvedParams = await params;
-  redirect(`/${resolvedParams.username}/${resolvedParams.area_slug}`);
-}
